b95b41e3a85bddb9ac65c547746922396461b598
nandu44-code <nanduganeshnair4444@gmail.com>
2024-01-17 17:13:03 +0530

    double linked list created

---
+++ linkedlist/DoubleLinkedlist.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head=None
+        self.temp=None
+    def insert(self,value):
+        n=Node(value)
+        if self.head is None:
+            self.head=n
+            self.temp=self.head
+        elif self.head.next is None:
+            self.head.next = n
+            n.prev=self.head
+            self.temp=n
+        else:
+            self.temp.next=n
+            n.prev=self.temp
+            self.temp=n
+    def reversePrint(self):
+        pointer=self.temp
+
+        while pointer:
+            print(pointer.data,'->',end='')
+            pointer=pointer.prev
+     
+    def display(self):
+        pointer=self.head
+
+        while pointer:
+            print(pointer.data,'->',end=' ')
+            pointer=pointer.next
+       
+        
+l=DoubleLinkedList()
+
+l.insert(2)
+l.insert(3)
+l.insert(4)
+l.insert(5)
+l.display()
+
+l.reversePrint()
